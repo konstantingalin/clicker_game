ba56b39aea55ba5153f038ff3b8c1cfa62714403
konstantingalin <the.it.promo@gmail.com>
2021-03-03 22:06:13 +0600

    lesson 2 done

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    [SerializeField] int money;
+    public int total_money;
+    public Text moneyText;
+
+    private void Start()
+    {
+        money = PlayerPrefs.GetInt("money");
+        total_money = PlayerPrefs.GetInt("total_money");
+    }
+
+    public void ButtonClick() 
+    {
+        money++;
+        total_money++;
+        PlayerPrefs.SetInt("money", money);
+        PlayerPrefs.SetInt("total_money", total_money);
+    }
+
+    public void toAchvments()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    void Update()
+    {
+        moneyText.text = money.ToString();
+    }
+}
